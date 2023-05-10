4ff926fa8a15891e89d956d3d779b5182a99ff8e
Uttam Makwana <uttamakwana4503@gmail.com>
2023-05-10 14:10:58 +0530

    Commented ShortBreak.jsx file

--- src/components/Shortbreak/ShortBreak.jsx
+++ src/components/Shortbreak/ShortBreak.jsx
@@ -3,6 +3,7 @@ import { StateContext } from "../../StateProvider";
 import "../clock.css";
 
 function ShortBreak() {
+  // Taking states value of time, getTime, isActive, setActive for displaying time and controlling the button
   const { time, getTime, isActive, setIsActive } = useContext(StateContext);
 
   return (
